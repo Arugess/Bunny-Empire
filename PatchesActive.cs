cccb073c6fe0d1773717a8f4762da6a4d29892ed
Arugess <georgemeharris@gmail.com>
2019-04-14 13:08:06 -0700

    Update PatchesActive.cs

--- PatchesActive.cs
+++ PatchesActive.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//This shows the number of actrivated carrot patches in the game through text.//
 
 public class PatchesActive : MonoBehaviour
 {
